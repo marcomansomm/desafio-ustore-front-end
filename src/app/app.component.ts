53f4f7d444e6513d226455545cfd6d1275a465c8
marcomansomm <marco.santos@tascomtecnologia.com.br>
2024-09-06 19:57:02 -0300

    feat/ add header and footer component in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './view/home/home.component';
-import { HeaderComponent } from './view/header/header.component';
-import { FooterComponent } from './view/footer/footer.component';
-import { NavComponent } from './view/nav/nav.component';
+import { HeaderComponent } from './views/header/header.component';
+import { FooterComponent } from './views/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    HomeComponent,
-    HeaderComponent,
-    FooterComponent,
-    NavComponent,
-  ],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
